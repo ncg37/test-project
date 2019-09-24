0a5c2a05ac2504f4144d840e04ed730a90217724
Nati Garcia <natigarcia86@gmail.com>
2019-09-24 13:39:59 -0400

    second commit - testing

--- analysis.R
+++ analysis.R
@@ -1,1 +1,12 @@
 mean (rnorm(100))
+
+#Para usar GIT si no veo el boton o para un proyecto viejo
+#Crea un repositorio GIT and commits los archivos.
+
+library(usethis)
+
+use_git()
+
+#Para conectar al GitHub
+
+use_github(protocol = "https")
